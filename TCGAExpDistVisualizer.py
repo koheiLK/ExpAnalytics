8a276323b69f4b3c8243d0f4b3fa108b9b8955cb
koheiLK <kohei.faith@gmail.com>
2016-12-17 09:10:03 +0900

    simplified

--- TCGAExpDistVisualizer.py
+++ TCGAExpDistVisualizer.py
@@ -1,28 +1,31 @@
-# -*- coding: utf-8 -*-
+#-*- coding: utf-8 -*-
 #TCGAのデータを読み込み,希望するある１つの遺伝子についての発現を正常と癌で二群に分けてboxplotで表示するプログラム
-
-import pandas as pd
-
-import matplotlib as mpl
-import matplotlib.pyplot as plt
 
 print('Please wait...')
 
-df=pd.read_csv('genomicMatrix',delimiter='\t',index_col=0)
-tdf=df.T
-genelist=list(df.index)
+import pandas as pd
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+
+_gdata = pd.read_csv('genomicMatrix', delimiter='\t', index_col=0)
+_cdata = pd.read_csv('clinical_data', delimiter='\t', index_col=0)
+
+gdata = _gdata.T
+cdata = _cdata['sample_type']
+genelist = list(_gdata.index)
+
 print('Please enter your gene of interest.')
-genename=input('>>')
+genename = input('>>')
 if not genename in genelist:
 	print('your GOI is not found')
 	exit()
-gdf=tdf[genename]
-cdata=pd.read_csv('clinical_data',delimiter='\t',index_col=0)
-tdf=cdata['sample_type']
-fdf=pd.concat([tdf,gdf],axis=1,join='inner')
+picked_gdata = gdata[genename]
+eval_df = pd.concat([cdata, picked_gdata], axis=1, join='inner')
 
-fig=plt.figure()
-ax=fig.add_subplot(111)
-bp=fdf.boxplot(column=genename,by='sample_type')
+fig = plt.figure()
+ax = fig.add_subplot(111)
+bp = eval_df.boxplot(column=genename, by='sample_type')
 plt.show()
+
 
